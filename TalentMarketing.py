7514f1503efd47604d4f1c60f23630bf6c4b8f47
shifang1994 <shifang5901@gmail.com>
2018-07-13 18:20:57 -0700

    Final Commit

--- TalentMarketing.py
+++ TalentMarketing.py
@@ -1,14 +1,18 @@
+### This is the implemetation of the UML class diagram for talent marketing saftware deisgn
+### Methods defined in the class should be revised when designing
+### the database interface layer (Data Access Object Layer)
+### fang shi, 2018-07-13
 
 
 class Skills():
-    def __init__(self, id, name,category):
-        self._id = id
+    def __init__(self, sid, name,category):
+        self._sid = sid
         self._name = name
         self._category = category
-    def setId(self,id):
-        self._id = id
+    def setId(self,sid):
+        self._sid = sid
     def getId(self):
-        return self._id
+        return self._sid
     def setName(self,name):
         self._name = name
     def getName(self):
@@ -28,11 +32,10 @@ class Project():
         self._duration = duration
         self._start_date = start_date
         self._end_date = end_date
-        self._required_skills = required_skills
+        self._required_skills = []
         self._location = location
         self._description = description
         self._is_complete = is_complete
-
         self._requests = []
         self._members = []
 
@@ -44,9 +47,10 @@ class Project():
         if employee in self._members:
             self._members.remove(employee)
 
-    def Request(self,employee):
+    def addRequest(self,employee):
         if employee not in self._requests:
             self._requests.append(employee)
+
     def getRequest(self):
         return self._requests
 
@@ -54,11 +58,19 @@ class Project():
 class Assessment():
     def __init__(self,project_id,endosed_skills,feedback,rating):
         self._project_id = project_id
-        self._endosed_skills = endosed_skills
+        self._endosed_skills = []
         self._feedback = feedback
         self._rating = rating
     def getEndosedSkills(self):
         return self._endosed_skills
+
+
+class SkillEndosement():
+    def __init__(self,id, project_id, sid):
+        self._id = id
+        self._project_id = project_id
+        self._sid = sid
+
 
 class User():
     def __init__(self,id,password,login_status,register_date,name,sex,birthday,role,email):
@@ -80,7 +92,7 @@ class ProjectOwner(User):
 
         self._projects = []
 
-    def PostProject(self, project):
+    def postProject(self, project):
         ### post the project
         self._projects.append(project)
 
@@ -89,23 +101,23 @@ class ProjectOwner(User):
             if proj.id == project_id:
                 print(proj.getRequests)
 
-    def Match(self,project_id, requested_employee):
+    def match(self,project_id, requested_employee):
         ### calculate the matching scores based on employee's interests and skills
         ### and the project's requirements
-        ##project.required_skills
+        ### project.required_skills
         pass
 
-    def Accept(self,project_id, requested_employee):
+    def accept(self,project_id, requested_employee):
         ### send message to employee ("Approval")
         for proj in self._projects:
             if proj.id == project_id:
                 proj._members.append(requested_employee)
 
-    def Deny(self,project_id, requested_employeee):
+    def deny(self,project_id, requested_employee):
         ### send message to employee ("Deny")
         pass
 
-    def Assess(self,employee, assessment):
+    def assess(self,employee, assessment):
         employee.completeProject(assessment)
 
 
@@ -141,6 +153,7 @@ class Employee(User):
         ### else, just return
         pass
 
+### add endosed skills into employee skillsets after completing the project
     def completeProject(self,assess_results):
         self._completed_projects.append(assess_results)
         for skill in assess_results.getEndosedSkills():
